f6b67df7e815f378893352b57ddf8fb22fb1e506
Phildez <pdesiletssds@gmail.com>
2022-04-14 11:32:51 -0400

    trigger done

---
+++ database/trigger.sql
@@ -0,0 +1,26 @@
+CREATE TABLE IF NOT EXISTS varieteaudit(
+	id serial not null,
+	nomlatin VARCHAR(30),
+	jardinid VARCHAR(10),
+	xparcelle NUMERIC(6,0),
+	yparcelle NUMERIC(6,0),
+	numerorang SERIAL,
+	entrydate timestamp,
+	PRIMARY KEY (id)
+);
+
+CREATE OR REPLACE FUNCTION variete_audit_log() RETURNS TRIGGER AS $miseenplace$
+	BEGIN
+		insert into varieteaudit (nomlatin, jardinid, xparcelle, yparcelle, numerorang, entrydate) values (new.nomlatin, new.jardinid, new.xparcelle, new.yparcelle, new.numerorang, current_timestamp);
+		return NEW;
+	END;
+$miseenplace$ LANGUAGE plpgsql;
+
+CREATE TRIGGER variete_audit_trigger AFTER INSERT ON miseenplace
+FOR EACH ROW EXECUTE PROCEDURE variete_audit_log();
+
+--Exécutez pour tester
+--INSERT INTO MiseEnPlace (NomLatin, JardinId, XParcelle, YParcelle, NumeroRang, MiseEnPlace, estBiologique)
+--VALUES ('Amelanchier canadensis', '1', 10000, 20000, 1, 'plant', FALSE);
+
+--select * from varieteaudit;
